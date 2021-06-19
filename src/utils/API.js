c2f3f6103c4b7a881ea7b317015c2d7e8083d141
Sean Fitzmartin <sfitzmartin22@gmail.com>
2021-06-19 11:29:23 -0400

    Updates

--- src/utils/API.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-    populateEmployees: function() {
-        return axios.get('https://randomuser.me/api/?results=200');
-    }
-};
